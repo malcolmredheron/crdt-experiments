c4bb236f5aa6bc1307fb4be85325251f5bc0cfe1
malcolm <m@redheron.com>
2022-07-29 18:51:11 +0200

    Include parcel in precommit so that we don't accidentally break it
    Not sure what the package-lock change is about.

--- dev/precommit.sh
+++ dev/precommit.sh
@@ -7,5 +7,8 @@ npx tsc --noEmit --project tsconfig.json # check types
 npx prettier --write --config .prettierrc "**"
 npx eslint --max-warnings=0 "src/**"
 npm run testOnce
+# Parcel's caching is buggy and will fail when we remove a test file.
+# If the build fails, try `rm -rf .parcel-cache/`.
+npx parcel build src/mochaInBrowswer/MochaInBrowser.html
 echo
 echo "All good"
